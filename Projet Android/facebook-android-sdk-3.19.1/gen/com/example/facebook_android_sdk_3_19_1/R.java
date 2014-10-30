38885a22f057000e7ae637b692f022f39e7b31ca
nicolas fougnies <nicolas.fougnies@gmail.com>
2014-10-30 15:10:46 +0100

    FacebookSDK

---
+++ Projet Android/facebook-android-sdk-3.19.1/gen/com/example/facebook_android_sdk_3_19_1/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.example.facebook_android_sdk_3_19_1;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
